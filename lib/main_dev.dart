4bc04fb42d3c7dcb29e43354fa3588e3c30fed81
silverTaurus11 <gayuhnh@gmail.com>
2026-01-05 14:19:21 +0700

    implement firebase

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options_dev.dart';
 
 import 'di/di.dart';
 import 'main.dart';
@@ -7,9 +12,24 @@ import 'main.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env.dev");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError =
+      FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  await FirebaseCrashlytics.instance
+      .setCrashlyticsCollectionEnabled(true);
+
   configureDependencies(
     baseUrl: dotenv.env['BASE_URL'] ?? "",
     token: dotenv.env['TOKEN'] ?? "",
   );
-  runApp(const MyApp());
+
+  runZonedGuarded(() {
+    runApp(const MyApp());
+  }, (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+  });
 }
